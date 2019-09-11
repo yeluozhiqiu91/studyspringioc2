f88d261483829b4b5718416131c171ccbe44360c
wangliujie <wangliujie@phone580.com>
2019-09-11 10:26:16 +0800

    fix(src): 添加属性

--- src/main/java/org/study/spring/ioc/SayService.java
+++ src/main/java/org/study/spring/ioc/SayService.java
@@ -5,9 +5,17 @@ package org.study.spring.ioc;
  * @Date: 2019/9/3 11:31
  */
 public class SayService {
+    private String mouth;
 
     public void say(){
         System.out.println("I am Spring");
     }
 
+    public String getMouth() {
+        return mouth;
+    }
+
+    public void setMouth(String mouth) {
+        this.mouth = mouth;
+    }
 }
